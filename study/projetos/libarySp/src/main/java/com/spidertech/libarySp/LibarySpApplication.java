f51f014bb0d3c09de7441e1b254f12213d4698fc
SpiderTheus <matemora80@gmail.com>
2025-06-19 23:02:53 -0300

    initial settings

---
+++ study/projetos/libarySp/src/main/java/com/spidertech/libarySp/LibarySpApplication.java
@@ -0,0 +1,13 @@
+package com.spidertech.libarySp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarySpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarySpApplication.class, args);
+	}
+
+}
---
+++ study/projetos/libarySp/src/test/java/com/spidertech/libarySp/LibarySpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spidertech.libarySp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibarySpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
